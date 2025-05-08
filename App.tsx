95dacb09b196d386f0cf3a49c6f6b664a7b4713f
Lee Minung <qxqxd454@naver.com>
2025-05-08 10:36:50 +0900

    커밋 푸쉬 테스트

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import HomeScreen from './screens/HomeScreen';
 import {Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
+  // 깃허브 커밋 푸쉬 테스트
   return (
     <View>
       <Text>여기는 홈이다ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ</Text>
