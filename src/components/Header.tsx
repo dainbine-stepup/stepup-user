a4fde77ca41beb81dfa5d4e51780636dc0c6b5e8
sungmin <holypp@naver.com>
2025-06-12 10:23:43 +0900

    가입일자 앱 설치 일자로 변경경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,65 +1,77 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import LinearGradient from "react-native-linear-gradient";
-
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import DeviceInfo from 'react-native-device-info';
 function Header() {
-  
-    return (
-        <LinearGradient
-            colors={["#5ee6dc", "#01b9e9"]}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 0 }}
-            style={styles.header}
-        >
-          <View style={styles.titleContainer}>
-            <Text style={styles.title}>브랜드 위즈</Text>
-          </View>
-          <View style={styles.dateContainer}>
-            <Text style={styles.dateTitleText}>가입일</Text>
-            <Text style={styles.dateText}>2025.03.03</Text>
-          </View>
-          
-        </LinearGradient>
-    );
+  const [installDate, setInstallDate] = useState('');
+  useEffect(() => {
+    const fetchInstallTime = async () => {
+      const installTime = await DeviceInfo.getFirstInstallTime(); // timestamp
+
+      const date = new Date(installTime);
+      const formatted = `${date.getFullYear()}.${String(
+        date.getMonth() + 1,
+      ).padStart(2, '0')}.${String(date.getDate()).padStart(2, '0')}`;
+      setInstallDate(formatted);
+    };
+
+    fetchInstallTime();
+  }, []);
+
+  return (
+    <LinearGradient
+      colors={['#5ee6dc', '#01b9e9']}
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 0}}
+      style={styles.header}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>브랜드 위즈</Text>
+      </View>
+      <View style={styles.dateContainer}>
+        <Text style={styles.dateTitleText}>가입일</Text>
+        <Text style={styles.dateText}>{installDate}</Text>
+      </View>
+    </LinearGradient>
+  );
 }
 const styles = StyleSheet.create({
   header: {
-    width: "100%",
+    width: '100%',
     height: 150,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#00c6ff",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00c6ff',
     borderRadius: 8,
   },
   titleContainer: {
-    width: "100%",
-    height: "50%",
+    width: '100%',
+    height: '50%',
     borderBottomWidth: 1,
-    borderColor: "#ADD8E6",
-    justifyContent: "center",
+    borderColor: '#ADD8E6',
+    justifyContent: 'center',
   },
   title: {
     paddingLeft: 20,
     fontSize: 32,
-    fontWeight: "bold",
-    color: "#FFF",
+    fontWeight: 'bold',
+    color: '#FFF',
   },
   dateContainer: {
-    width: "100%",
-    height: "50%",
+    width: '100%',
+    height: '50%',
     paddingLeft: 20,
     paddingTop: 10,
-    flexDirection: "row",
+    flexDirection: 'row',
     gap: 10,
   },
   dateTitleText: {
-    color: "#C7F6FD",
+    color: '#C7F6FD',
   },
   dateText: {
-    color: "#FFF",
+    color: '#FFF',
     fontSize: 16,
-    fontWeight: "bold",
-  }
+    fontWeight: 'bold',
+  },
 });
 
 export default Header;
